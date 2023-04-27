1ac2a019cdda62cb8c04e836e881ac96b53031e6
Michael-Dawkins <michaeldawfr@gmail.com>
2023-04-27 18:28:50 +0200

    switchToData

--- index.ts
+++ index.ts
@@ -3,25 +3,99 @@ import './style.css';
 import {
   of,
   map,
-  Observable,
   delay,
   switchMap,
-  BehaviorSubject,
   tap,
   MonoTypeOperatorFunction,
   EMPTY,
-  interval,
   ObservableInput,
   OperatorFunction,
   ObservedValueOf,
+  interval,
+  timer,
+  combineLatest,
+  ObservableInputTuple,
 } from 'rxjs';
-import { catchError, exhaustMap, retry, shareReplay } from 'rxjs/operators';
+import {
+  catchError,
+  combineLatestWith,
+  exhaustMap,
+  sample,
+  shareReplay,
+  take,
+  withLatestFrom,
+} from 'rxjs/operators';
+
+const data1$ = of(1);
+const data2Slow$ = of(2).pipe(delay(5000));
+const data3Reccurent$ = timer(0, 4000);
+
+// simlate issue with withLatestFrom
+// it drops the slow observables that were subscribed to earl once source starts
+// AND it is missing a take(1) behaviour
+/*
+of(0)
+  .pipe(delay(5000))
+  .pipe(
+    tap(() => console.log('emission in chain')),
+    withLatestFrom(
+      data1$.pipe(tap((v) => console.log('data1 emmited: ', v))),
+
+      //This is subscribed to at declaration, but get ignored as it is slower than the source
+      data2Slow$.pipe(tap((v) => console.log('data2Slow emmited: ', v)))
+    )
+  )
+  .subscribe(() => {
+    console.log('subscriber was called');
+  });
+  */
+
+timer(5000, 20000)
+  .pipe(delay(5000))
+  .pipe(
+    tap((v) => console.log('emission in chain: ', v)),
+    switchToLatest([
+      data1$.pipe(tap((v) => console.log('data1 emmited: ', v))),
+
+      //This is subscribed to at declaration, but get ignored as it is slower than the source
+      data2Slow$.pipe(tap((v) => console.log('data2Slow emmited: ', v))),
+
+      data3Reccurent$.pipe(
+        tap((v) => console.log('data3Reccurent emmited: ', v))
+      ),
+    ])
+  )
+  .subscribe(([data1, data2Slow, data3Reccurent]) => {
+    console.log(
+      'subscriber was called, batch of values captured: data1: ' +
+        data1 +
+        '  data2slow: ' +
+        data2Slow +
+        '  data3Reccurent: ' +
+        data3Reccurent
+    );
+  });
+
+export function switchToLatest<A extends readonly unknown[]>(
+  observables: readonly [...ObservableInputTuple<A>]
+) {
+  return switchMapSafe(() => combineLatest(observables).pipe(take(1)));
+}
+
+/*
+possibly simpler typings that works on older rx versions
+
+export function switchToLatest<A>(observables: [...Observable<A>[]]) {
+  return switchMapSafe(() => combineLatest(observables).pipe(take(1)))
+}
+
+*/
 
 /*
   Like switchMap, but, when an error occurs, it is swallowed silently
   Pass in an errorHandler to deal with the error
 */
-export function switchMapKeepAlive<T, O extends ObservableInput<any>>(
+export function switchMapSafe<T, O extends ObservableInput<any>>(
   fn: (value: T, index: number) => O,
   errorHandler?: (error: any) => void
 ): OperatorFunction<T, ObservedValueOf<O>> {
@@ -32,7 +106,7 @@ export function switchMapKeepAlive<T, O extends ObservableInput<any>>(
         if (errorHandler) {
           errorHandler(err);
         } else {
-          console.error('Unhandled error in switchMapKeepAlive:');
+          console.error('Unhandled error in switchMapSafe:');
           console.error(err);
           console.error(new Error().stack);
         }
@@ -67,6 +141,7 @@ export function exhaustMapSafe<T, O extends ObservableInput<any>>(
   });
 }
 
+/*
 interval(500)
   .pipe(
     exhaustMapSafe(
@@ -83,6 +158,8 @@ const userClicksOnButton$ = of('click');
 userClicksOnButton$.pipe(exhaustMapSafe(() => callApi())).subscribe((value) => {
   console.log('obtained: ' + value);
 });
+
+*/
 
 function callApi() {
   return of('data').pipe(
